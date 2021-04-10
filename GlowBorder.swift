8e828f15202baa5f1a3a7a795620dd333a342eef
Samuel Do <samdo@stanford.edu>
2021-04-10 14:05:01 -0700

    Added glow border

---
+++ GlowBorder.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct GlowBorder: ViewModifier {
+    var color: Color
+    var lineWidth: Int
+    
+    func body(content: Content) -> some View {
+        applyShadow(content: AnyView(content), lineWidth: lineWidth)
+    }
+    
+    func applyShadow(content: AnyView, lineWidth: Int) -> AnyView {
+        if lineWidth == 0 {
+            return content
+        } else {
+            return applyShadow(content: AnyView(content.shadow(color: color, radius: 1)), lineWidth: lineWidth - 1)
+        }
+    }
+}
+
+extension View {
+    func glowBorder(color: Color, lineWidth: Int) -> some View {
+        self.modifier(GlowBorder(color: color, lineWidth: lineWidth))
+    }
+}
